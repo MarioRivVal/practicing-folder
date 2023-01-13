3d07f3f17c222915474b386f621a82541fc52226
Mario Valverde Rivera <rival.moro@hotmail.it>
2023-01-13 23:37:18 +0000

    first commit, building a temp converter

---
+++ Cel-Far converter-temp/script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// const tempCel = Number (prompt ("Temperatura Celsius?"));
+// const tempFahr = (tempCel * (9/5)) + 32;  
+// //FORMULA PARA FAHRENHEIT - CELSIUS - (Fº - 32) * (5/9)
+// alert ( `la temperatura en Fahrenheit es ${tempFahr}`);
+
+ const resultEl = document.querySelector('.result');
+ const celciusTempEl = document.querySelector('.temp-1');
+ const summitEl = document.querySelector('.btn-summit');
+
+ 
+
+
+
+ summitEl.addEventListener('click', function(){
+ 
+
+ })
+
