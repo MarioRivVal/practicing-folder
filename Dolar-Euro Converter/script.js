9847cf511d9c6748e5f5e96f48de9a97caec73d8
Mario Valverde Rivera <rival.moro@hotmail.it>
2023-01-18 00:10:42 +0000

    first commit, creating a dolar-euro converter

---
+++ Dolar-Euro Converter/script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+//adding elements
+const currencyOneEl = document.querySelector('.currency--1');
+const currencyTwoEl = document.querySelector('.currency--2');
+const amountEl = document.querySelector('.amount');
+const answerEl = document.querySelector('.answer');
+const btnSubmit = document.querySelector('.btn-submit');
+const btnSwitchEl = document.querySelector('.btn-switch-logo');
+
+
+answerEl.textContent= "0"
+amountEl.value= "6"
+
+console.log(answerEl.textContent);
+console.log(amountEl.value);
+
+//changing currency function
+
+// const changCurrency = function ()
