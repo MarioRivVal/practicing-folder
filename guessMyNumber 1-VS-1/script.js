94d044466026f4c09e1594c880bbdf9522c5a387
Mario Valverde Rivera <rival.moro@hotmail.it>
2023-01-23 18:51:01 +0000

    stuck in the process to define the winner

--- guessMyNumber 1-VS-1/script.js
+++ guessMyNumber 1-VS-1/script.js
@@ -3,6 +3,8 @@
 //selecting variables
 const btnRollE = document.querySelector(".btn-roll");
 const btnNewE = document.querySelector(".btn-newgame");
+const playerBox0E = document.querySelector(".container-player--0");
+const playerBox1E = document.querySelector(".container-player--1");
 const scorePlayer0E = document.querySelector(".score-player--0");
 const scorePlayer1E = document.querySelector(".score-player--1");
 const activeDiceE = document.querySelector(".dice-photo--0");
@@ -18,6 +20,8 @@ const init = function () {
   scorePlayer1E.textContent = 0;
   activeDiceE.classList.add("hidden");
   document.querySelector(".guess-number").value = false;
+  playerBox0E.classList.add("active-player");
+  playerBox1E.classList.remove("active-player");
 };
 init();
 
@@ -28,55 +32,65 @@ const switchPlayer = function () {
   count = 0;
   currentScore = 0;
   document.querySelector(".guess-number").value = false;
-
+  playerBox0E.classList.toggle("active-player");
+  playerBox1E.classList.toggle("active-player");
 };
 
 // roll click listener
 btnRollE.addEventListener("click", function () {
-  activeDiceE.classList.remove("hidden");
   if (playing) {
     const guessE = Number(document.querySelector(".guess-number").value);
-    count++;
-    if (count < 6) {
+    if (guessE) {
+      activeDiceE.classList.remove("hidden");
+      count++;
       const dice = Math.trunc(Math.random() * 6) + 1;
       activeDiceE.src = `img/dice-${dice}.png`;
 
-      console.log(`count ${count}`);
-
-      if (guessE === dice) {
-        switch (count) {
-          case 1:
-            currentScore = 5;
-            break;
-          case 2:
-            currentScore = 4;
-            break;
-          case 3:
-            currentScore = 3;
-            break;
-          case 4:
-            currentScore = 2;
-            break;
-          case 5:
-            currentScore = 1;
-            break;
-          default:
-            break;
+      if (count < 6) {
+        if (guessE === dice) {
+          switch (count) {
+            case 1:
+              currentScore = 5;
+              break;
+            case 2:
+              currentScore = 4;
+              break;
+            case 3:
+              currentScore = 3;
+              break;
+            case 4:
+              currentScore = 2;
+              break;
+            case 5:
+              currentScore = 1;
+              break;
+            default:
+              break;
+          }
+          scores[activePlayer] = scores[activePlayer] + currentScore;
+          document.querySelector(`.score-player--${activePlayer}`).textContent =
+            scores[activePlayer];
+          switchPlayer();
         }
-        scores[activePlayer] = scores[activePlayer] + currentScore;
-        document.querySelector(`.score-player--${activePlayer}`).textContent =
-          scores[activePlayer];
-        console.log(`score ${currentScore}`);
-        console.log(scores[activePlayer]);
-        switchPlayer();
-        console.log(`player active ${activePlayer}`);
       }
-    }
-    if (count == 5) {
-      switchPlayer();
-      console.log(`player active ${activePlayer}`);
-    }
-  } // if one of the player get 20 the game is over
+      if (count == 5) {
+        switchPlayer();
+      }
+    } 
+  }
 });
 
+// New game buttom
 btnNewE.addEventListener("click", init);
+
+
+if (scores[activePlayer] >= 5) {
+  playing = false;
+
+  document
+    .querySelector(`.container-player--${activePlayer}`)
+    .classList.add("player-winner");
+  document
+    .querySelector(`.container-player--${activePlayer}`)
+    .classList.remove("active-player");
+}
