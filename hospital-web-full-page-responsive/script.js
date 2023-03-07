b85021fab08fd58294c715ee97f907bb1a3adc79
Mario Valverde Rivera <rival.moro@hotmail.it>
2023-03-07 10:17:03 +0000

    scroll behavior added

--- hospital-web-full-page-responsive/script.js
+++ hospital-web-full-page-responsive/script.js
@@ -1,5 +1,4 @@
-"use strict"
-
+"use strict";
 
 //------- TO GET THE YEAR ----- //
 
@@ -13,6 +12,17 @@ yearEl.textContent = currentYear;
 const mobileNavEl = document.querySelector(".open-close-btns");
 const navEl = document.querySelector(".main-nav");
 
-mobileNavEl.addEventListener("click",function(){
-navEl.classList.toggle("open-nav")
-})
+mobileNavEl.addEventListener("click", function () {
+  navEl.classList.toggle("open-nav");
+});
+
+//------- AUTOMATLY CLOSE THE MOBILE-NAV ----- //
+const allLinksMenuEl = document.querySelectorAll(".nav-link");
+
+allLinksMenuEl.forEach(function (link) {
+  link.addEventListener("click", function () {
+    if (link.classList.contains("nav-link")) {
+      navEl.classList.toggle("open-nav");
+    }
+  });
+});
